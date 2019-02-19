a32ffe25f7edd5ebb2cb697276a5001c50b2ce4b
RysynGaming <rysyn.gaming@gmail.com>
2019-02-18 22:38:33 -0800

    player movement implemented
    implemented player movement with restrictions

--- Galaxy Shooter/Assets/Game/Scripts/Player.cs
+++ Galaxy Shooter/Assets/Game/Scripts/Player.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start ()
+    private float baseSpeed = 10f;
+
+    void Start ()
     {
-		
+        Debug.Log(this.tag + " has spawned!");
+        transform.position = new Vector3(0, 0, 0);
 	}
 	
-	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+
+        movement();
+
+    }
+
+    private void movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * Time.deltaTime * baseSpeed * horizontalInput);
+        transform.Translate(Vector3.up * Time.deltaTime * baseSpeed * verticalInput);
+
+        if (transform.position.x > 9f)
+        {
+            transform.position = new Vector3(9f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -9f)
+        {
+            transform.position = new Vector3(-9f, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.y > 2f)
+        {
+            transform.position = new Vector3(transform.position.x, 2f, transform.position.z);
+        }
+        else if (transform.position.y < -5f)
+        {
+            transform.position = new Vector3(transform.position.x, -5f, transform.position.z);
+        }
+
+    }
 }
